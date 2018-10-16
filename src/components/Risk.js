77e96b248fabd4f9055bfe537361f0c643955d2d
Menno <mennodevries1@msn.com>
2018-10-16 12:39:57 +0200

    Data converted to JSON & Correction Menu

--- src/components/Risk.js
+++ src/components/Risk.js
@@ -1,26 +1,101 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import data from '../assets/data.json';
 
 class Risk extends Component {
 
   render() {
-    // JSON file van maken
-    const form = [{'id':'One', 'category':'Algemeen', 'description':'test 1'}, {'id':'Two', 'category':'Werk & Opleiding', 'description':'test 2'}];
 
-    const menuItems = form.map((form) =>
-    <div className="card shadow-sm" key={form.id}>
-      <div className="card-header" id={`heading${form.id}`}>
-        <button className="btn btn-link collapsed" data-toggle="collapse" data-target={`#collapse${form.id}`} aria-expanded="false" aria-controls={`collapse${form.id}`}>
-          {form.category}
-        </button>
-      </div>
-      <div id={`collapse${form.id}`} className="collapse" aria-labelledby={`heading${form.id}`} data-parent="#accordion">
-        <div className="card-body">
-          {form.description}
+    //----Return amount of questions----//
+        let questionsAmount = 0;
+
+        for (let i = 0; i < data.length; i++) {
+          for (let j = 0; j < data[i].questions.length; j++) {
+            questionsAmount++;
+          }
+        }
+
+    //----Return amount of questions not filled in----//
+
+
+        let unfilledQuestionsAmount = 0;
+        //var selectCollection = document.getElementsByTagName('select').length;
+        //const selectCollection = node.getElementsByTagName('select');
+          //{console.log(selectCollection.length)}
+
+        /*for (var i = 0; i < selectCollection.length; i++) {
+
+          selectCollection[i].addEventListener("click", function() {
+            unfilledQuestionsAmount++;
+          });
+        }
+
+        document.getElementsByTagName('select').onclick = function() {
+            console.log('Hello');
+        }*/
+
+
+        /*var value = $('.form-control').filter(function () {
+          return this.value !== '';
+        });
+
+        if (value.length>=0 && (value.length !== selectCollection)) {
+
+        }*/
+
+        /*let unfilledQuestionsAmount = 3;
+
+        let selectCollection = document.getElementsByClassName('form-control');
+        for (var i = 0; i < selectCollection.length; i++) {
+          selectCollection[i].selectedIndex = unfilledQuestionsAmount;
+          {console.log(unfilledQuestionsAmount)}
+        } */
+
+
+      /*  var value = selectField.options[selectField.selectedIndex].value;
+        if (value === "") {
+          unfilledQuestionsAmount++;
+        } */
+
+//----Return ID, category, labels and options----//
+    const menuItems = data.map(data =>
+      <div className="card shadow-sm" key={data.id}>
+        <div className="card-header" id={`heading${data.id}`}>
+          <button className="btn btn-link collapsed font-weight-bold" data-toggle="collapse" data-target={`#collapse${data.id}`} aria-expanded="false" aria-controls={`collapse${data.id}`}>
+            {data.category}
+          </button>
+        </div>
+        <div id={`collapse${data.id}`} className="collapse" aria-labelledby={`heading${data.id}`} data-parent="#accordion">
+          <div className="card-body">
+            {data.questions.map(question =>
+            <>
+              <label key={question} className="font-weight-bold">{question.label}</label>
+              <select className="form-control mb-4" required>
+                <option disabled selected hidden value="">-</option>
+                {question.options.map(option =>
+                  <option key={option} className="d-block">{option}</option>
+                )}
+              </select>
+            </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    );
+    )
 
+    const tableContent = data.map(data =>
+      <tbody>
+        <tr>
+          <th scope="row" className="font-weight-normal">{data.category}</th>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+      </tbody>
+    )
+
+
+
+//----Return front-end----//
     return (
       <div>
         <div className="container-fluid">
@@ -34,7 +109,17 @@ class Risk extends Component {
             <div className="col-8">
               <div className="card shadow-sm">
                 <div className="card-body text-justify">
-                  <h5 className="card-title">Test</h5>
+                  <h5 className="card-title">Resultaten</h5>
+                  <table class="table table-striped text-center">
+                    <thead>
+                      <tr>
+                        <th scope="col">Cateogrie</th>
+                        <th scope="col">Individuele risicotaxatie</th>
+                        <th scope="col">Vragen ingevuld</th>
+                      </tr>
+                    </thead>
+                    {tableContent}
+                  </table>
                 </div>
               </div>
             </div>
